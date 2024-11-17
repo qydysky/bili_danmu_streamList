c49749f33ef424a0c3054e57070bfc0643648938
qydysky <qydysky@foxmail.com>
2024-11-17 19:48:38 +0800

    1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,18 +2,156 @@
 import Axios from 'axios'
 import { setupCache } from 'axios-cache-interceptor';
 export default {
-    loadFileLists(){
+    data() {
+        return {
+            search: "",
+            loading: true,
+            loopLoading: false,
+            disabledLoadFileList: false,
+            tableData: []
+        }
+    },
+    computed: {
+        loading: {
+            get() {
+                return this.loading
+            },
+            set(loading) {
+                this.loading = loading
+            }
+        },
+        filterTableData() {
+            let data = this.tableData.sort((a,b)=>{
+                if(a.startT>b.startT)return 1
+                if(a.startT==b.startT)return 0
+                if(a.startT<b.startT)return -1
+            })
+            return data
+        },
+        search: {
+            get() {
+                return this.search
+            },
+            set(value) {
+                this.search = value
+            }
+        }
+    },
+    methods: {
+        rowClick(data) {
+            window.open("player/?ref="+data.path+(data.format?"&format="+data.format:""))
+        },
+        hotRowClick(data) {
+            window.open("player/?ref="+data.path+(data.format?"&format="+data.format:"")+"&st="+data.st+"m&dur="+data.dur+"m")
+        },
+        colspan({
+            row,
+            column,
+            rowIndex,
+            columnIndex,
+        }) {
+            if(row.firstSpan && columnIndex == 0)return {rowspan: 2, colspan: 1}
+            if(row.colspan && columnIndex == 0)return {rowspan: 1, colspan: 0}
+            if(row.colspan && columnIndex == 1)return {rowspan: 1, colspan: 10}
+            return {rowspan: 1, colspan: 1}
+        },
+        loadFileList(){
+            if(this.loopLoading || this.disabledLoadFileList)return console.log("skip")
+            this.loopLoading = true
+            let that = this
+            const axios = setupCache(Axios.create());
+            axios.get('filePath?size=20&skip='+((this.tableData.length>0)?this.tableData.length-1:0))
+            .then(function (response) {
+                const load = async (data) => {
+                    that.disabledLoadFileList = !data || data.length < 20
+                    for (let index = 0; data && index < data.length; index++) {
+                        const element = data[index]
+                        let result2 = []
+                        await axios.get('danmuCountPerMin?ref='+element.path)
+                        .then(function (response) {
+                            if(!response || response.length==0 || !response.data || response.data.length==0)return
+                            let avg = array => (array&&array.length>0)?(array.reduce((a,b)=>a+b)/array.length):0;
+                            let avgC = avg(response.data) 
+                            let result = []
+                            response.data.forEach((v,k)=>{
+                                if(v>=avgC*1.5){
+                                    result.push(k)
+                                }
+                            })
+                            if(result.length==0)return
+                            let mergedOP = -1
+                            let m = a => a>0.5?a-0.5:a
+                            if(result.length==1)result2.push({st:m(result[0]),dur:1.5,path:element.path,format:element.format})
+                            else result.reduce((a,b)=>{
+                                let merge = a>=b-3
+                                if(merge){
+                                    if(mergedOP==-1){
+                                        mergedOP = a
+                                    }
+                                } else if(mergedOP==-1) {
+                                    result2.push({st:m(a),dur:1.5,path:element.path,format:element.format})
+                                } else {
+                                    result2.push({st:m(mergedOP),dur:b-a+1.5,path:element.path,format:element.format})
+                                    mergedOP = -1
+                                }
+                                return b
+                            })
+                        })
+                        .finally(()=>{
+                            if(result2.length>0)that.tableData.push({
+                                startLiveT: element.startLiveT,
+                                format:element.format,
+                                uname: element.uname,
+                                name: element.name,
+                                path: element.path,
+                                qn: element.qn,
+                                avgOnline: max(element.onlinesPerMin),
+                                startT: element.startT,
+                                hot: result2
+                            })
+                            else {
+                                that.tableData.push({
+                                    startLiveT: element.startLiveT,
+                                    format:element.format,
+                                    uname: element.uname,
+                                    name: element.name,
+                                    path: element.path,
+                                    qn: element.qn,
+                                    avgOnline: max(element.onlinesPerMin),
+                                    startT: element.startT
+                                })
+                            }
+                        })
+                    }
+                    if(that.disabledLoadFileList)that.tableData.push({startLiveT: "加载完毕"})
+                    that.loopLoading = false
+                };
+
+                let res = response.data
+                let max = array => (array&&array.length>0)?Math.round(array.reduce((a,b)=>a>b?a:b)):0;
+                if (res.code == 0)load(res.data)
+                else console.error(res.message)
+            })
+            .then(function (params) {
+                setTimeout(()=>{
+                    that.loading = false
+                },300)
+            })
+        },
+    },
+    mounted() {
         if(this.loopLoading || this.disabledLoadFileList)return console.log("skip")
         this.loopLoading = true
         let that = this
-        setupCache(Axios.create()).get('filePath?size=20&skip='+((this.tableData.length>0)?this.tableData.length-1:0))
+        const axios = setupCache(Axios.create());
+        axios.get('filePath?size=20')
         .then(function (response) {
             const load = async (data) => {
                 that.disabledLoadFileList = !data || data.length < 20
                 for (let index = 0; data && index < data.length; index++) {
                     const element = data[index]
                     let result2 = []
-                    await setupCache(Axios.create()).get('danmuCountPerMin?ref='+element.path)
+                    await axios.get('danmuCountPerMin?ref='+element.path)
                     .then(function (response) {
                         if(!response || response.length==0 || !response.data || response.data.length==0)return
                         let avg = array => (array&&array.length>0)?(array.reduce((a,b)=>a+b)/array.length):0;
@@ -69,6 +207,7 @@ export default {
                         }
                     })
                 }
+                if(that.disabledLoadFileList)that.tableData.push({startLiveT: "加载完毕"})
                 that.loopLoading = false
             };
 
@@ -82,66 +221,6 @@ export default {
                 that.loading = false
             },300)
         })
-    },
-    data() {
-        return {
-            search: "",
-            loading: true,
-            loopLoading: false,
-            disabledLoadFileList: false,
-            tableData: []
-        }
-    },
-    computed: {
-        loading: {
-            get() {
-                return this.loading
-            },
-            set(loading) {
-                this.loading = loading
-            }
-        },
-        filterTableData() {
-            let data = this.tableData.sort((a,b)=>{
-                if(a.startT>b.startT)return 1
-                if(a.startT==b.startT)return 0
-                if(a.startT<b.startT)return -1
-            })
-            return data
-        },
-        search: {
-            get() {
-                return this.search
-            },
-            set(value) {
-                this.search = value
-            }
-        }
-    },
-    methods: {
-        rowClick(data) {
-            window.open("player/?ref="+data.path+(data.format?"&format="+data.format:""))
-        },
-        hotRowClick(data) {
-            window.open("player/?ref="+data.path+(data.format?"&format="+data.format:"")+"&st="+data.st+"m&dur="+data.dur+"m")
-        },
-        colspan({
-            row,
-            column,
-            rowIndex,
-            columnIndex,
-        }) {
-            if(row.firstSpan && columnIndex == 0)return {rowspan: 2, colspan: 1}
-            if(row.colspan && columnIndex == 0)return {rowspan: 1, colspan: 0}
-            if(row.colspan && columnIndex == 1)return {rowspan: 1, colspan: 10}
-            return {rowspan: 1, colspan: 1}
-        },
-        loadFileList(){
-            return loadFileLists()
-        },
-    },
-    mounted() {
-        loadFileLists()
     }
 }
 
