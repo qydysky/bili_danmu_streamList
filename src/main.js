009fe7bc138bd5f740d3bd8e3130147479d52a13
qydysky <qydysky@foxmail.com>
2024-11-20 02:42:27 +0800

    1

--- src/main.js
+++ src/main.js
@@ -6,16 +6,15 @@ import 'element-plus/dist/index.css'
 const app = createApp(App)
 app.directive( 'vTablescroll', {
 	mounted(el,binding) {
+        const sign = 50
         const selectwrap = el.querySelector('.el-scrollbar__wrap')
         if (selectwrap == null) return
         selectwrap.scrollFn = function () {
-            const sign = 50
             const scrollDistance = this.scrollHeight - this.scrollTop - this.clientHeight
             if (scrollDistance <= sign)binding.value()
-            return scrollDistance <= sign
         }
         selectwrap.addEventListener( 'scroll', selectwrap.scrollFn)
-        while(selectwrap.scrollFn()){}
+        while((selectwrap.scrollHeight - selectwrap.scrollTop - selectwrap.clientHeight)<sign){binding.value()}
     },
     unmounted(el){
         const selectwrap = el.querySelector('.el-scrollbar__wrap')
